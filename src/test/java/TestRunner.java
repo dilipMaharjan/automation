d7403b5466769b3d31e13e7c795a564bb63c9690
dilipMaharjan <dilipmaharjann@gmail.com>
2022-01-04 13:21:56 -0600

    step defs, test runner, reports

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,6 +3,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = {"StepDefinitions"})
+@CucumberOptions(features = "src/test/resources/features",
+        glue = {"StepDefinitions"},
+        monochrome = true,
+        plugin = {"pretty","html:target/reports/htmlReport","json:target/reports/jsonReport","junit:target/reports/junitReport"})
 public class TestRunner {
 }
